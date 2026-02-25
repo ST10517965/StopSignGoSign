1ccc367c9c12f0f2043f95b9fabb2e514bcd5705
ST10517965 <ST10517965@myemeris.edu.za>
2026-02-25 09:01:31 +0200

    Second Commit

---
+++ src/stopsigngosign/StopSignGoSign.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package stopsigngosign;
+
+/**
+ *
+ * @author ST10517965
+ */
+public class StopSignGoSign {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        //The two figures will be displayed exactly as they are since the
+        //alignment does not include any escape sequences.
+        System.out.println("              -----------");
+        System.out.println ();
+        System.out.println ("    ********" + "\t\t    xxxxxxxx");
+        System.out.println ("  *          *" + "\t\t  x          x");
+        System.out.println (" *    STOP    *" + "\t\t x     GO     x");
+        System.out.println ("  *          *" + "\t\t  x          x");
+        System.out.println ("    ********" + "\t\t    xxxxxxxx");
+        System.out.println ("       ** " + "\t\t       xx");
+        System.out.println ("       ** " + "\t\t       xx");
+        System.out.println ("       ** " + "\t\t       xx");
+        System.out.println ("       ** " + "\t\t       xx");
+        System.out.println ("      **** " + "\t\t      xxxx");
+        System.out.println ("   **********" + "\t\t   xxxxxxxxxx");
+        System.out.println ("|---------------------------------------|");
+        System.out.println ("|---------------------------------------|");
+    }
+    
+}
